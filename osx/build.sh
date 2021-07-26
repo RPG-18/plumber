55dd6b746f8cb1783103dc401f37dfd3a4f2d622
Dmitry Mukhitov <dmitriy.mukhitov@gmail.com>
2021-07-27 00:39:16 +0300

    try to build with travis

--- osx/build.sh
+++ osx/build.sh
@@ -5,4 +5,4 @@ cmake -B build -S . -DCMAKE_BUILD_TYPE=Release
 cmake --build build --config Release
 
 cp build/kafkaui osx/kafkaui.app/Contents/MacOS
-macdeployqt osx/kafkaui.app -appstore-compliant -executable=osx/kafkaui.app/Contents/MacOS/kafkaui -qmldir=/usr/local/opt/qt/share/qt/qml -dmg
+macdeployqt osx/kafkaui.app -appstore-compliant -executable=osx/kafkaui.app/Contents/MacOS/kafkaui -qmldir=/usr/local/opt/qt/share/qt/qml -fs=APFS -dmg
