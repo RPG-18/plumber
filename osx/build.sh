40708481cbbf4410166b622ddeec1423ca5e261a
Dmitry Mukhtiov <dmitriy.mukhitov@gmail.com>
2021-07-26 02:49:05 -0700

    fix errors when usage qt from brew

--- osx/build.sh
+++ osx/build.sh
@@ -5,5 +5,4 @@ cmake -B build -S . -DCMAKE_BUILD_TYPE=Release
 cmake --build build --config Release
 
 cp build/kafkaui osx/kafkaui.app/Contents/MacOS
-install_name_tool -add_rpath "@executable_path/../Frameworks" osx/kafkaui.app/Contents/MacOS/kafkaui
-macdeployqt osx/kafkaui.app -appstore-compliant -qmldir=/usr/local/opt/qt/share/qt/qml -dmg > /dev/null 2>&1
+macdeployqt osx/kafkaui.app -appstore-compliant -executable=osx/kafkaui.app/Contents/MacOS/kafkaui -qmldir=/usr/local/opt/qt/share/qt/qml -dmg
