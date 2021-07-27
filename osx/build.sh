1ed680506f074351e78272489ee7425607a3ec0c
Dmitry Mukhtiov <dmitriy.mukhitov@gmail.com>
2021-07-27 00:47:15 -0700

    macdeployqt verbose

--- osx/build.sh
+++ osx/build.sh
@@ -5,4 +5,8 @@ cmake -B build -S . -DCMAKE_BUILD_TYPE=Release
 cmake --build build --config Release
 
 cp build/kafkaui osx/kafkaui.app/Contents/MacOS
-macdeployqt osx/kafkaui.app -appstore-compliant -executable=osx/kafkaui.app/Contents/MacOS/kafkaui -qmldir=/usr/local/share/qt/qml -fs=APFS -dmg
+macdeployqt osx/kafkaui.app -appstore-compliant \
+  -executable=osx/kafkaui.app/Contents/MacOS/kafkaui \
+  -libpath=/usr/local/lib \
+  -qmldir=/usr/local/share/qt/qml -fs=APFS -dmg \
+  -verbose=3
