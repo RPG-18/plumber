dbdf12fc54e3ed368bf687f65f61dd69f7ab9711
Dmtry Mukhitov <dmitriy.mukhitov@gmail.com>
2021-09-08 20:57:08 +0300

    build package on osx (#10)

--- osx/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-
-export PATH="/usr/local/bin:$PATH"
-cmake -B build -S . -DCMAKE_BUILD_TYPE=Release
-cmake --build build --config Release
--- osx/build_dmg.sh
+++ osx/build_dmg.sh
@@ -3,10 +3,9 @@
 cmake -B build -S . -DCMAKE_BUILD_TYPE=Release
 cmake --build build --config Release
 
-cp build/kafkaui osx/kafkaui.app/Contents/MacOS
-macdeployqt osx/kafkaui.app \
+macdeployqt build/kafkaui.app \
   -appstore-compliant \
-  -executable=$HOME/kafkaui/osx/kafkaui.app/Contents/MacOS/kafkaui \
+  -executable=build/kafkaui.app/Contents/MacOS/kafkaui \
   -libpath=$HOME/Qt/6.1.2/macos/lib \
   -qmldir=$HOME/Qt/6.1.2/macos/qml \
   -fs=APFS -dmg
