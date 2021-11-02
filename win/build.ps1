23c050c57873a8fc5b71fc5530f5e60762a56848
Dmitry Mukhitov <dmitriy.mukhitov@gmail.com>
2021-11-02 23:06:09 +0300

    AppVeyor: upgrade Qt 6.2.1

--- win/build.ps1
+++ win/build.ps1
@@ -1,8 +1,8 @@
-$Env:Path += ";C:\Qt\6.2.0\msvc2019_64\bin"
+$Env:Path += ";C:\Qt\6.2.1\msvc2019_64\bin"
 $Env:VCINSTALLDIR = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC"
 
 & cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=C:/tools/vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_BUILD_TYPE=Release
 & cmake --build build --config Release
-& C:/Qt/6.2.0/msvc2019_64/bin/windeployqt.exe --qmldir C:/Qt/6.2.0/msvc2019_64/qml build/Release
+& C:/Qt/6.2.1/msvc2019_64/bin/windeployqt.exe --qmldir C:/Qt/6.2.1/msvc2019_64/qml build/Release
 Rename-Item  -Path build/Release -NewName plumber
 Compress-Archive -Path build/plumber -DestinationPath plumber_win64.zip -CompressionLevel Optimal
