3d3cd910b086f1a6fa0aa254afaacd19f61b8a9e
Dmtry <dmitriy.mukhitov@gmail.com>
2021-11-04 19:52:14 +0300

    windows: change path

--- win/build.ps1
+++ win/build.ps1
@@ -4,6 +4,6 @@ $Env:VCINSTALLDIR = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Communi
 & cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=C:/tools/vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_BUILD_TYPE=Release
 & cmake --build build --config Release
 & C:/Qt/6.2.1/msvc2019_64/bin/windeployqt.exe --qmldir C:/Qt/6.2.1/msvc2019_64/qml build/Release
+Copy-Item -Path C:/Qt/6.2.1/msvc2019_64/qml/Qt/labs/platform/* -Destination build/Release/Qt/labs -Recurse
 Rename-Item  -Path build/Release -NewName plumber
-Copy-Item -Path C:/Qt/6.2.1/msvc2019_64/qml/Qt/labs/platform/* -Destination build/plumber/Release/Qt/labs -Recurse
 Compress-Archive -Path build/plumber -DestinationPath plumber_win64.zip -CompressionLevel Optimal
