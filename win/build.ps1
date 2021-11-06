635a3b9ca673aacd59dc339d29d6f4df0e8842d3
Dmtry <dmitriy.mukhitov@gmail.com>
2021-11-06 13:17:32 +0300

    win: remove pdb

--- win/build.ps1
+++ win/build.ps1
@@ -6,5 +6,6 @@ $Env:VCINSTALLDIR = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Communi
 & C:/Qt/6.2.1/msvc2019_64/bin/windeployqt.exe --qmldir C:/Qt/6.2.1/msvc2019_64/qml build/Release
 New-Item -ItemType Directory -Force -Path build/Release/Qt/labs/platform
 Copy-Item -Path C:/Qt/6.2.1/msvc2019_64/qml/Qt/labs/platform/* -Destination build/Release/Qt/labs/platform/ -Recurse
+Remove-Item build/Release/Qt/labs/platform -Include *.pdb
 Rename-Item  -Path build/Release -NewName plumber
 Compress-Archive -Path build/plumber -DestinationPath plumber_win64.zip -CompressionLevel Optimal
