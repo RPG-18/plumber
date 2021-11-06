372e6cd437e478a91b5d1f1c8259d3503cecbe7e
Dmtry <dmitriy.mukhitov@gmail.com>
2021-11-06 12:19:09 +0300

    win: fix Qt Labs platform

--- osx/build_dmg.sh
+++ osx/build_dmg.sh
@@ -4,7 +4,7 @@ cmake -B build -S . -DCMAKE_BUILD_TYPE=Release
 cmake --build build --config Release
 
 mkdir -p build/plumber.app/Contents/Resources/qml/Qt/labs
-cp -R $HOME/Qt/6.2.1/macos/qml/Qt/labs/platform build/plumber.app/Contents/Resources/qml/Qt/labs
+cp -R $HOME/Qt/6.2.1/macos/qml/Qt/labs/platform build/plumber.app/Contents/Resources/qml/Qt/labs/platform
 
 macdeployqt build/plumber.app \
   -appstore-compliant \
