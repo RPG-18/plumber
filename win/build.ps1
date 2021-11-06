67069f49eec78a19cfae94d10a400335bacde3ab
Dmtry <dmitriy.mukhitov@gmail.com>
2021-11-06 12:56:12 +0300

    win: i am think fix Qt Labs platform

--- win/build.ps1
+++ win/build.ps1
@@ -4,6 +4,7 @@ $Env:VCINSTALLDIR = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Communi
 & cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=C:/tools/vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_BUILD_TYPE=Release
 & cmake --build build --config Release
 & C:/Qt/6.2.1/msvc2019_64/bin/windeployqt.exe --qmldir C:/Qt/6.2.1/msvc2019_64/qml build/Release
-Copy-Item -Path C:/Qt/6.2.1/msvc2019_64/qml/Qt/labs/platform/* -Destination build/Release/Qt/labs -Recurse
+New-Item -ItemType Directory -Force -Path build/Release/Qt/labs/platform
+Copy-Item -Path C:/Qt/6.2.1/msvc2019_64/qml/Qt/labs/platform/* -Destination build/Release/Qt/labs/platform/ -Recurse
 Rename-Item  -Path build/Release -NewName plumber
 Compress-Archive -Path build/plumber -DestinationPath plumber_win64.zip -CompressionLevel Optimal
