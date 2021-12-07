ae7e33bec861535c388c3704675f80f178f40e78
Dmitry Mukhitov <dmitriy.mukhitov@gmail.com>
2021-12-08 00:01:33 +0300

    upgrade Qt for AppVeyor

--- win/build.ps1
+++ win/build.ps1
@@ -1,11 +1,11 @@
-$Env:Path += ";C:\Qt\6.2.1\msvc2019_64\bin"
+$Env:Path += ";C:\Qt\6.2.2\msvc2019_64\bin"
 $Env:VCINSTALLDIR = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC"
 
 & cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=C:/tools/vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_BUILD_TYPE=Release
 & cmake --build build --config Release
-& C:/Qt/6.2.1/msvc2019_64/bin/windeployqt.exe --qmldir C:/Qt/6.2.1/msvc2019_64/qml build/Release
+& C:/Qt/6.2.2/msvc2019_64/bin/windeployqt.exe --qmldir C:/Qt/6.2.2/msvc2019_64/qml build/Release
 New-Item -ItemType Directory -Force -Path build/Release/Qt/labs/platform
-Copy-Item -Path C:/Qt/6.2.1/msvc2019_64/qml/Qt/labs/platform/* -Destination build/Release/Qt/labs/platform/ -Recurse
+Copy-Item -Path C:/Qt/6.2.2/msvc2019_64/qml/Qt/labs/platform/* -Destination build/Release/Qt/labs/platform/ -Recurse
 Remove-Item build/Release/Qt/labs/platform -Include *.pdb
 Rename-Item  -Path build/Release -NewName plumber
 Compress-Archive -Path build/plumber -DestinationPath plumber_win64.zip -CompressionLevel Optimal
