70568d2da02ddb76f14e5bd79a17e20534e20c61
Dmtry <dmitriy.mukhitov@gmail.com>
2022-10-20 22:46:57 +0300

    fix: windeployqt change qml

--- win/build.ps1
+++ win/build.ps1
@@ -3,7 +3,7 @@ $Env:VCINSTALLDIR = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Communi
 
 & cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=C:/tools/vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_BUILD_TYPE=Release
 & cmake --build build --config Release
-& C:/Qt/6.3.1/msvc2019_64/bin/windeployqt.exe --qmldir C:/Qt/6.3.1/msvc2019_64/qml build/Release
+& C:/Qt/6.3.1/msvc2019_64/bin/windeployqt.exe --qmldir src/qml build/Release
 New-Item -ItemType Directory -Force -Path build/Release/Qt/labs/platform
 Copy-Item -Path C:/Qt/6.3.1/msvc2019_64/qml/Qt/labs/platform/* -Destination build/Release/Qt/labs/platform/ -Recurse
 Remove-Item build/Release/Qt/labs/platform -Include *.pdb
